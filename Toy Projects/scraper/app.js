da421074d76332e097e6a4736e0978d723abe068
Christopher Li <li.christopher.jr@gmail.com>
2019-07-11 21:09:44 +0800

    Delete scraper project

--- Toy Projects/scraper/app.js
+++
@@ -1,8 +0,0 @@
-const scrapeIt = require("scrape-it");
-
-scrapeIt("https://coursehunters.net/course/polnoe-vvedenie-v-react-v5", {
-  "lesson-names": ".lessons-name "
-}).then(({ data, response }) => {
-  console.log(`Status Code: ${response.statusCode}`);
-  console.log(data);
-});
